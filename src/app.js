7a6f9e7ecf796bc85678f1a8ce9689c661f403c3
upendtu <upendtu@gmail.com>
2019-08-12 15:48:12 +0900

    push code

---
+++ src/app.js
@@ -0,0 +1,114 @@
+
+class ProductList extends React.Component {
+    state = {
+        products: [],
+      };
+    
+      componentDidMount() {
+        this.setState({ products: Seed.products });
+      }
+
+      handleProductUpVote = (productId) => {
+        const nextProducts = this.state.products.map((product) => {
+          if (product.id === productId) {
+            return Object.assign({}, product, {
+              votes: product.votes + 1,
+            });
+          } else {
+            return product;
+          }
+        });
+        this.setState({
+          products: nextProducts,
+        });
+      }
+
+
+    render() {
+
+        const products = this.state.products.sort((a, b) => (
+            b.votes - a.votes
+        ));
+
+        const productComponents = products.map((product) => (
+            <Product
+              key={'product-' + product.id}
+              id={product.id}
+              title={product.title}
+              description={product.description}
+              url={product.url}
+              votes={product.votes}
+              submitterAvatarUrl={product.submitterAvatarUrl}
+              productImageUrl={product.productImageUrl}
+              onVote={this.handleProductUpVote}
+            />
+          ));
+
+        return (
+            <div className="container">
+                <h1>Popular products</h1>
+                <hr />
+                {productComponents}
+            </div>
+        );
+    }
+}
+
+class Product extends React.Component {
+
+    constructor() {
+        super();
+        this.passTheId = this.passTheId.bind(this);
+    }
+
+    passTheId() {
+        console.log('Here Id will be passed');
+        this.props.onVote(this.props.id);
+    }
+
+
+    render() {
+        return (
+          <div className='container'>
+            <div className="row">
+            <div className='col-md-12'>
+
+
+                <div className="main">
+                <div className="image">  
+                    <img src={this.props.productImageUrl} />
+                </div> 
+
+                <div className='header'>
+                    <a onClick={this.passTheId}>
+                        <i className='fa fa-2x fa-caret-up' />
+                    </a>
+                    {this.props.votes}
+                </div>
+                <div className='description'>
+                    <a href={this.props.url}>
+                        {this.props.title}
+                    </a>
+                    <p>
+                        {this.props.description}
+                    </p>
+                </div>
+              <div className='extra'>
+                <span>Submitted by:</span>
+                <img
+                  className='avatar'
+                  src={this.props.submitterAvatarUrl}
+                />
+              </div>
+              </div>
+
+
+            </div>
+            </div>
+          </div>
+        );
+      }
+}
+
+
+ReactDOM.render(<ProductList />, document.getElementById('content'));
---
+++ src/seed.js
@@ -0,0 +1,47 @@
+window.Seed = (function () {
+    function generateVoteCount() {
+      return Math.floor((Math.random() * 50) + 15);
+    }
+  
+    const products = [
+      {
+        id: 1,
+        title: 'Yellow Pail',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'src/images/avatars/daniel.jpg',
+        productImageUrl: 'src/images/products/image-aqua.png',
+      },
+      {
+        id: 2,
+        title: 'Supermajority: The Fantasy Congress League',
+        description: 'Earn points when your favorite politicians pass legislation.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'src/images/avatars/kristy.png',
+        productImageUrl: 'src/images/products/image-rose.png',
+      },
+      {
+        id: 3,
+        title: 'Tinfoild: Tailored tinfoil hats',
+        description: 'We already have your measurements and shipping address.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'src/images/avatars/veronika.jpg',
+        productImageUrl: 'src/images/products/image-steel.png',
+      },
+      {
+        id: 4,
+        title: 'Haught or Naught',
+        description: 'High-minded or absent-minded? You decide.',
+        url: '#',
+        votes: generateVoteCount(),
+        submitterAvatarUrl: 'src/images/avatars/molly.png',
+        productImageUrl: 'src/images/products/image-yellow.png',
+      },
+    ];
+  
+    return { products: products };
+  }());
+  
